b60deda533d2fc07e2002047c242491329b2a142
nemesis530 <153313631+nemesis530@users.noreply.github.com>
2024-01-07 03:24:09 +0100

    Max vel + Borders
    implementata velocità massima
    implementati bordi camera per far rimbalzare il giocatore

--- Assets/Scripts/Game Scene/PlayerController.cs
+++ Assets/Scripts/Game Scene/PlayerController.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float initialMoveSpeed = 90f;
+    public float initialMoveSpeed = 100f;
+    public float maxSpeed = 500f; // Velocità massima
     public GameController gameController;
     private Rigidbody2D rb;
 
@@ -12,9 +13,9 @@ public class PlayerController : MonoBehaviour
         if (rb == null)
         {
             Debug.LogError("Rigidbody2D component not found on the player.");
-            return; // Interrompe ulteriori esecuzioni se rb è null
+            return;
         }
-        rb.gravityScale = 0; // Assicurati che non ci sia gravità applicata
+        rb.gravityScale = 0;
     }
 
     void Update()
@@ -23,6 +24,9 @@ public class PlayerController : MonoBehaviour
         {
             AddMovement();
         }
+
+        CheckBoundsAndBounce();
+        LimitSpeed();
     }
 
     private void AddMovement()
@@ -35,7 +39,33 @@ public class PlayerController : MonoBehaviour
         Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 moveDirection = ((Vector2)(worldPoint - transform.position)).normalized;
 
-        // Aggiunge la velocità alla velocità corrente
         rb.velocity += moveDirection * initialMoveSpeed;
+    }
+
+    private void CheckBoundsAndBounce()
+    {
+        float margin = 50f;
+
+        Vector2 minCameraBounds = (Vector2)Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)) + new Vector2(margin, margin);
+        Vector2 maxCameraBounds = (Vector2)Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0)) - new Vector2(margin, margin);
+
+        Vector2 playerPosition = new Vector2(transform.position.x, transform.position.y);
+
+        if (playerPosition.x < minCameraBounds.x || playerPosition.x > maxCameraBounds.x)
+        {
+            rb.velocity = new Vector2(-rb.velocity.x, rb.velocity.y);
+            transform.position = new Vector2(Mathf.Clamp(playerPosition.x, minCameraBounds.x, maxCameraBounds.x), playerPosition.y);
+        }
+
+        if (playerPosition.y < minCameraBounds.y || playerPosition.y > maxCameraBounds.y)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y);
+            transform.position = new Vector2(playerPosition.x, Mathf.Clamp(playerPosition.y, minCameraBounds.y, maxCameraBounds.y));
+        }
+    }
+
+    private void LimitSpeed()
+    {
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
     }
 }
